f941d87fe3362cffda037ceff39f3b58a12a5343
Kyle Nelson <kyle.st.aubyn@gmail.com>
2019-11-13 11:11:53 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Splash/>
     </div>
   );
+}
+
+function Splash(){
+
+  const[text, setText] = useState(false);
+  const[choice, setChoice] = useState(false);
+  let b1 = "Go Alone";
+  let b2 = "Find a Buddy";
+
+
+
+  function handleClick(choice){
+    setText(true);
+  }
+
+  if(text){
+    return(
+      <div className="outer">
+        <div className="container">
+          <p>Thank you for the feedback</p>
+        </div>
+      </div>
+    );
+  }
+  else{
+
+  
+
+    return (
+        <div className="outer">
+          <div className="container">
+            <div className="sad" onClick={() => handleClick(b1)}><p>Go Alone</p></div>
+            <div className="happy" onClick={() => handleClick(b2)}><p>Find a Buddy</p></div>
+          </div>
+          <p>{text}</p>
+        </div>
+    );
+  }
 }
 
 export default App;
