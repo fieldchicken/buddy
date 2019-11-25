e9da1a14fa90c180c209e636cd8f6889435420d6
Kyle Nelson <kyle.st.aubyn@gmail.com>
2019-11-25 09:10:22 +0800

    update UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, {useState} from 'react';
-import ReactGA from 'react-ga';
+// import ReactGA from 'react-ga';
 import logo from './logo.svg';
 import './App.css';
 
 
 
-function initializeReactGA() {
-  ReactGA.initialize('UA-152383455-1');
-  ReactGA.pageview('/');
-}
+// function initializeReactGA() {
+//   ReactGA.initialize('UA-152383455-1');
+//   ReactGA.pageview('/');
+// }
 
 function App() {
   return (
@@ -31,18 +31,18 @@ function Splash(){
 
 
   function handleClick(choice){
-    setText(true);
-    if (choice === b1){
-      ReactGA.event({
-        category: "Alone Button Clicked",
-        action: "User pressed the alone button",
-      });
-    }else if (choice === b2){
-      ReactGA.event({
-        category: "Buddy Button Clicked",
-        action: "User pressed the buddy button",
-      });
-    }
+    // setText(true);
+    // if (choice === b1){
+    //   ReactGA.event({
+    //     category: "Alone Button Clicked",
+    //     action: "User pressed the alone button",
+    //   });
+    // }else if (choice === b2){
+    //   ReactGA.event({
+    //     category: "Buddy Button Clicked",
+    //     action: "User pressed the buddy button",
+    //   });
+    // }
     
   }
 
@@ -61,12 +61,35 @@ function Splash(){
 
     return (
         <div className="outer">
-          <p>What is your choice?</p>
-          <div className="container">
-            <div className="sad" onClick={() => handleClick(b1)}><p>Go Alone</p></div>
-            <div className="happy" onClick={() => handleClick(b2)}><p>Find a Buddy</p></div>
+          <div class="fixed one">
+            <p>What is your choice?</p>
+            <div className="container">
+              <div className="sad" onClick={() => handleClick(b1)}><p>Go Alone</p></div>
+              <div className="happy" onClick={() => handleClick(b2)}><p>Find a Buddy</p></div>
+            </div> 
           </div>
-          <p>{text}</p>
+          <div class="scroll">
+            <h3>
+              Feeling down and crummy? 
+            </h3>
+            <h3>
+              Well join our growing community and find your buddy!
+            </h3>
+          </div>
+          <div class="fixed two"></div>
+          <div class="scroll">
+            <h3>
+              Are you overwhelemed with all the different apps and websites for events? 
+            </h3>
+            <h3>
+              Buddie is the all-in-one solution for you!
+            </h3>
+          </div>
+          <div class="fixed three">
+            <h3>
+              Join us today!
+            </h3>
+          </div>
         </div>
     );
   }
