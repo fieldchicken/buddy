4e15ed12867b3274de50cb3aa7676cc543d3f408
Kyle Nelson <kyle.st.aubyn@gmail.com>
2019-12-07 15:15:28 +0800

    adding analytics'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, {useState} from 'react';
-// import ReactGA from 'react-ga';
+import ReactGA from 'react-ga';
 import logo from './logo.svg';
 import './App.css';
 
 
 
-// function initializeReactGA() {
-//   ReactGA.initialize('UA-152383455-1');
-//   ReactGA.pageview('/');
-// }
+function initializeReactGA() {
+  ReactGA.initialize('UA-152383455-1');
+  ReactGA.pageview('/');
+}
 
 function App() {
   return (
@@ -31,19 +31,19 @@ function Splash(){
 
 
   function handleClick(choice){
-    // setText(true);
-    // if (choice === b1){
-    //   ReactGA.event({
-    //     category: "Alone Button Clicked",
-    //     action: "User pressed the alone button",
-    //   });
-    // }else if (choice === b2){
-    //   ReactGA.event({
-    //     category: "Buddy Button Clicked",
-    //     action: "User pressed the buddy button",
-    //   });
-    // }
-    
+    setText(true);
+    if (choice === b1){
+      ReactGA.event({
+        category: "Alone Button Clicked",
+        action: "User pressed the alone button",
+      });
+    }else if (choice === b2){
+      ReactGA.event({
+        category: "Buddy Button Clicked",
+        action: "User pressed the buddy button",
+      });
+    }
+
   }
 
   if(text){
@@ -57,7 +57,7 @@ function Splash(){
   }
   else{
 
-  
+
 
     return (
         <div className="outer">
@@ -66,11 +66,11 @@ function Splash(){
             <div className="container">
               <div className="sad" onClick={() => handleClick(b1)}><p>Go Alone</p></div>
               <div className="happy" onClick={() => handleClick(b2)}><p>Find a Buddy</p></div>
-            </div> 
+            </div>
           </div>
           <div class="scroll">
             <h3>
-              Feeling down and crummy? 
+              Feeling down and crummy?
             </h3>
             <h3>
               Well join our growing community and find your buddy!
@@ -79,7 +79,7 @@ function Splash(){
           <div class="fixed two"></div>
           <div class="scroll">
             <h3>
-              Are you overwhelemed with all the different apps and websites for events? 
+              Are you overwhelemed with all the different apps and websites for events?
             </h3>
             <h3>
               Buddie is the all-in-one solution for you!
